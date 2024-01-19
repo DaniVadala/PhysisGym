27378aa1efc23341851acfccd836533a9d8c5e49
Dani-Vadala <dani@studiohakuna.com>
2024-01-19 20:41:57 -0300

    add Our Classes Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
+
 
 
 import { useEffect, useState } from "react";
