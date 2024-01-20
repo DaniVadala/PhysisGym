0b4d54a8bdd958d3190d0a6fbebacff17eba9a1d
Dani-Vadala <dani@studiohakuna.com>
2024-01-19 22:02:59 -0300

    Footer Added

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,31 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+<img className="h-10" src="https://upload.wikimedia.org/wikipedia/commons/3/36/Pakua.svg" alt="" />        
+  <p className="my-5">
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
+            orci ut habitant laoreet. Iaculis tristique.
+          </p>
+          <p>© Physis Gym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p>Ullamcorper vivamus</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p>(000)4000000</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
